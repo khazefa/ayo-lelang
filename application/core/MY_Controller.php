23042c229233db6083754a13e87f75ad6cc957cd
Sigit Prayitno <cybergitt@gmail.com>
2019-06-23 02:16:49 +0700

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -15,34 +15,38 @@ class MY_Controller extends MX_Controller
 		parent::__construct();
 	}
 	/**
-	 * Layouting for frontend
+	 * Layouting for Frontside
 	 */
-	public function digiLayout($data = [], $view = null, $headerInfo = NULL, $footerInfo = NULL)
+	public function _renderFrontView($data = [], $view = null, $headerInfo = NULL, $footerInfo = NULL)
 	{
-		$this->load->view('layouts/frontend/header', $headerInfo);
+		$this->load->view('layouts/frontside/header', $headerInfo);
 		if ($view) {
 			$this->load->view($view, $data);
 		}
-		$this->load->view('layouts/frontend/footer', $footerInfo);
+		$this->load->view('layouts/frontside/footer', $footerInfo);
 	}
 	/**
-	 * Layouting for Backend
+	 * Layouting for Backside
 	 */
-	public function digiAdminLayout($data = [], $view = null, $headerInfo = NULL, $footerInfo = NULL)
+	public function _renderBackView($data = [], $view = null, $headerInfo = NULL, $footerInfo = NULL)
 	{
-		$this->load->view('layouts/backend/header', $headerInfo);
+		$this->load->view('layouts/backside/header', $headerInfo);
 		if ($view) {
 			$this->load->view($view, $data);
 		}
-		$this->load->view('layouts/backend/footer', $footerInfo);
+		$this->load->view('layouts/backside/footer', $footerInfo);
 	}
 	/**
 	 * Load a single View
 	 */
-	public function digiView($data = [], $view = null)
+	public function _renderView($data = [], $base = null, $view = null)
 	{
 		if ($view) {
-			$this->load->view($view, $data);
+			if ($base) {
+				$this->load->view($base.'/'.$view, $data);
+			} else {
+				$this->load->view($view, $data);
+			}
 		}
 	}
 }
