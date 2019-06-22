7f8400944b6fcaa65cd5e0b0fe676b685352d0fd
Sigit Prayitno <cybergitt@gmail.com>
2019-06-23 01:39:31 +0700

    Create Front_Controller.php

---
+++ application/core/Front_Controller.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * Class Front_Controller.php.
+ * Desc: Pre-Logged-in stuff
+ * @author: Sigit Prayitno
+ * @email: cybergitt@gmail.com
+ */
+
+class Front_Controller extends MY_Controller
+{
+	public function __construct()
+	{
+		parent::__construct();
+	}
+	//just in case you need to add something here
+}
