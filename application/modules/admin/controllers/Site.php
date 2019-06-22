a028abc120fd78e9014f991e7802316762978218
Sigit Prayitno <cybergitt@gmail.com>
2019-06-23 02:16:54 +0700

    Create Site.php

---
+++ application/modules/admin/controllers/Site.php
@@ -0,0 +1,56 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * Class Site.php.
+ * Desc: Extending Back_Controller. Check /core/Back_Controller
+ * @author: Sigit Prayitno
+ * @email: cybergitt@gmail.com
+ */
+
+class Site extends Back_Controller
+{
+	private $view_dir = 'admin/dashboard';
+
+	public function __construct()
+	{
+		parent::__construct();
+		//load session library
+		$this->load->library('session');
+		// $this->load->model('admin/Auth_model', 'MLog');
+	}
+
+	/**
+	 * Show Dashboard interface
+	 */
+	public function index()
+	{
+		//check for login status
+		// if (!$this->session->userdata('logged_in')) {
+		// 	redirect('/admin/signin');
+		// 	exit;
+		// }
+
+		$this->global['pageTitle'] = 'Dashboard';
+		$this->global['contentHeader'] = 'Dashboard';
+		$this->global['contentTitle'] = 'Dashboard';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$data = array();
+		// $this->digiAdminLayout($data, $this->view_dir, $this->global);
+	}
+
+	/**
+	 * Show Login interface
+	 */
+	public function signin()
+	{
+		//check for login status
+		if ($this->session->userdata('logged_in')) {
+			redirect('/admin/Site/');
+			exit;
+		}
+
+		$data = array();
+		$this->digiView($data, 'admin/signin');
+	}
+}
