09d7ee9402403e760afdd8b04bfebccdc3821f5b
Sigit Prayitno <cybergitt@gmail.com>
2019-06-24 16:16:02 +0700

    Update Kategori.php

--- application/modules/admin/controllers/Kategori.php
+++ application/modules/admin/controllers/Kategori.php
@@ -40,4 +40,117 @@ class Kategori extends Back_Controller
         $data['records'] = $rs;
 		$this->digiAdminLayout($data, $this->view_dir.'index', $this->global);
     }
+
+    /**
+     * This function is used to load the add new form
+     */
+    function add()
+    {   
+        $this->global['pageTitle'] = "Tambah Data";
+        $this->global['contentHeader'] = 'Tambah Data';
+        $this->global['contentTitle'] = 'Tambah Data';
+        $this->global['name'] = $this->accName;
+        $this->global['role'] = $this->accRole;
+
+		$data = array();
+        $this->digiAdminLayout($data, $this->view_dir.'create', $this->global);
+    }
+    
+    /**
+     * This function is used to add new data to the system
+     */
+    function create()
+    {
+        $fnama = $this->input->post('fnama', TRUE);
+        $falias = $this->input->post('falias', TRUE);
+        $fdeskripsi = $this->input->post('fdeskripsi', TRUE);
+
+        $dataInfo = array('alias_kategori'=> $falias, 'nama_kategori'=>$fnama, 'deskripsi_kategori'=>$fdeskripsi);
+        $count = $this->MKategori->check_data_exists(array('alias_kategori' => $falias));
+        if ($count > 0)
+        { 
+            setFlashData('error', 'Data sudah ada, harap isi dengan data lainnya');
+            redirect('admin/kategori/add');
+        }
+        else
+        { 
+            $result = $this->MKategori->insert_data($dataInfo);
+        
+            if($result > 0)
+            {
+                setFlashData('success', 'Data telah sukses ditambahkan');
+                redirect('admin/kategori');
+            }
+            else
+            {
+                setFlashData('error', 'Data telah gagal ditambahkan');
+                redirect('admin/kategori/add');
+            }
+        }
+    }
+
+    /**
+     * This function is used load edit information
+     * @param $fkey : Optional : This is data unique key
+     */
+    function edit($fkey = NULL)
+    {
+        if($fkey == NULL)
+        {
+			redirect('admin/kategori');
+        }
+
+        $this->global['pageTitle'] = "Edit Data";
+        $this->global['contentHeader'] = 'Edit Data';
+        $this->global['contentTitle'] = 'Edit Data';
+        $this->global['name'] = $this->accName;
+        $this->global['role'] = $this->accRole;
+
+		$rs = $this->MKategori->get_data_info($fkey);
+        $data['records'] = $rs;
+        $this->digiAdminLayout($data, $this->view_dir.'edit', $this->global);
+    }
+    
+    /**
+     * This function is used to edit the data information
+     */
+    function update()
+    {
+        $fid = $this->input->post('fid', TRUE);
+        $fnama = $this->input->post('fnama', TRUE);
+        $falias = $this->input->post('falias', TRUE);
+        $fdeskripsi = $this->input->post('fdeskripsi', TRUE);
+
+        $dataInfo = array('alias_kategori'=> $falias, 'nama_kategori'=>$fnama, 'deskripsi_kategori'=>$fdeskripsi);
+        $result = $this->MKategori->update_data($dataInfo, $fid);
+        if($result == true)
+        {
+            setFlashData('success', 'Data is successfully updated');
+			redirect('admin/kategori');
+        }
+        else
+        {
+			setFlashData('error', 'Failed to update data');
+			redirect('admin/kategori/edit'.$fid);
+        }
+    }
+    
+    /**
+     * This function is used to delete the data
+     * @return boolean $result : TRUE / FALSE
+     */
+    function delete($fkey = NULL)
+    {
+        $result = $this->MKategori->delete_data($fkey);
+
+        if($result > 0)
+        {
+            setFlashData('success', 'Data is successfully deleted');
+        }
+        else
+        {
+            setFlashData('error', 'Failed to delete data');
+        }
+        redirect('admin/kategori');
+    }
 }
