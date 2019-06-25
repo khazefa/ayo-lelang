39e2a3a07b8872709b67ad7a5f1e3f6a506aa5a1
Sigit Prayitno <cybergitt@gmail.com>
2019-06-26 01:08:42 +0700

    Update any_code_helper.php

--- application/helpers/any_code_helper.php
+++ application/helpers/any_code_helper.php
@@ -42,4 +42,15 @@ if (!function_exists('format_rupiah')) {
 	}
 }
 
+if (!function_exists('tgl_indo')) {
+	function tgl_indo($tgl)
+	{
+		$ubah = gmdate($tgl, time() + 60 * 60 * 8);
+		$pecah = explode("-", $ubah);
+		$tanggal = $pecah[2];
+		$bulan = bulan($pecah[1]);
+		$tahun = $pecah[0];
+		return $tanggal . ' ' . $bulan . ' ' . $tahun;
+	}
+}
 ?>
