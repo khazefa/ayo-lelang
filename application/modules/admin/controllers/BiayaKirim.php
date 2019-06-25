2a5540f1a97ea57b805b318bdb4e16f73027cf07
Sigit Prayitno <cybergitt@gmail.com>
2019-06-25 23:02:00 +0700

    Create BiayaKirim.php

---
+++ application/modules/admin/controllers/BiayaKirim.php
@@ -0,0 +1,140 @@
+<?php
+/**
+ * Class BiayaKirim.php.
+ * Desc: Class for every BiayaKirim function purposes
+ * @author: Sigit Prayitno
+ * @email: cybergitt@gmail.com
+ */
+
+class BiayaKirim extends Back_Controller
+{
+	private $view_dir = 'admin/biaya-kirim/';
+
+	/**
+	 * This is default constructor of the class
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+		$this->isLoggedIn();
+		$this->load->model('admin/BiayaKirim_model', 'MBiayaKirim');
+		$this->load->model('admin/Kota_model', 'MKota');
+	}
+
+	/**
+	 * This function is used to load the menu index
+	 */
+	public function index()
+	{
+		$rs = array();
+		$arrWhere = array();
+		$arrOrder = array('id_biaya_kirim' => 'ASC');
+		$limit = 0;
+
+		$this->global['pageTitle'] = 'Biaya Kirim';
+		$this->global['contentHeader'] = 'Biaya Kirim';
+		$this->global['contentTitle'] = 'Biaya Kirim';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$rs = $this->MBiayaKirim->get_data_join_kota($arrWhere, $arrOrder, $limit);
+		$data['records'] = $rs;
+		$this->digiAdminLayout($data, $this->view_dir . 'index', $this->global);
+	}
+
+	/**
+	 * This function is used to load the add new form
+	 */
+	function add()
+	{
+		$this->global['pageTitle'] = "Tambah Data";
+		$this->global['contentHeader'] = 'Tambah Data';
+		$this->global['contentTitle'] = 'Tambah Data';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$arrWhere = array();
+		$arrOrder = array('nama_kota' => 'ASC');
+		$limit = 0;
+
+		$rs_kota = $this->MKota->get_data($arrWhere, $arrOrder, $limit);
+		$data['records_kota'] = $rs_kota;
+		$this->digiAdminLayout($data, $this->view_dir . 'create', $this->global);
+	}
+
+	/**
+	 * This function is used to add new data to the system
+	 */
+	function create()
+	{
+		$fkota = $this->input->post('fkota', TRUE);
+		$fharga = $this->input->post('fharga', TRUE);
+
+		$dataInfo = array('id_kota' => $fkota, 'jumlah_biaya_kirim' => $fharga);
+		$result = $this->MBiayaKirim->insert_data($dataInfo);
+
+		if ($result > 0) {
+			setFlashData('success', 'Data telah sukses ditambahkan');
+			redirect('admin/biaya-kirim');
+		} else {
+			setFlashData('error', 'Data telah gagal ditambahkan');
+			redirect('admin/biaya-kirim/add');
+		}
+	}
+
+	/**
+	 * This function is used load edit information
+	 * @param $fkey : Optional : This is data unique key
+	 */
+	function edit($fkey = NULL)
+	{
+		if ($fkey == NULL) {
+			redirect('admin/biaya-kirim');
+		}
+
+		$this->global['pageTitle'] = "Edit Data";
+		$this->global['contentHeader'] = 'Edit Data';
+		$this->global['contentTitle'] = 'Edit Data';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$rs = $this->MBiayaKirim->get_data_info($fkey);
+		$data['records'] = $rs;
+		$this->digiAdminLayout($data, $this->view_dir . 'edit', $this->global);
+	}
+
+	/**
+	 * This function is used to edit the data information
+	 */
+	function update()
+	{
+		$fid = $this->input->post('fid', TRUE);
+		$fnama = $this->input->post('fnama', TRUE);
+
+		$dataInfo = array('nama_kota' => $fnama);
+		$result = $this->MBiayaKirim->update_data($dataInfo, $fid);
+		if ($result == true) {
+			setFlashData('success', 'Data is successfully updated');
+			redirect('admin/biaya-kirim');
+		} else {
+			setFlashData('error', 'Failed to update data');
+			redirect('admin/biaya-kirim/edit' . $fid);
+		}
+	}
+
+	/**
+	 * This function is used to delete the data
+	 * @return boolean $result : TRUE / FALSE
+	 */
+	function delete($fkey = NULL)
+	{
+		$result = $this->MBiayaKirim->delete_data($fkey);
+
+		if ($result > 0) {
+			setFlashData('success', 'Data is successfully deleted');
+		} else {
+			setFlashData('error', 'Failed to delete data');
+		}
+		redirect('admin/biaya-kirim');
+	}
+}
