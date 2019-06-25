8f02da0b4721ca3f8f44c4f98212ca379c3bb331
Sigit Prayitno <cybergitt@gmail.com>
2019-06-25 22:24:15 +0700

    Create create.php

---
+++ application/modules/admin/views/kota/create.php
@@ -0,0 +1,63 @@
+<!-- Main content -->
+<section class="content">
+    <div class="row">
+        <div class="col-md-8 offset-md-4">
+            <div class="box box-default">
+                <div class="box-header with-border">
+                    <a href="javascript: history.go(-1)"><i class="fa fa-reply"></i></a>
+                    <h3 class="box-title"><?= $contentTitle;?></h3>
+
+                    <div class="box-tools pull-right">
+                        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+                    </div>
+                </div>
+                <!-- /.box-header -->
+                <form class="form-horizontal" action="<?php echo base_url('admin/kota/create');?>" method="POST" role="form">
+                    <div class="box-body">
+                        <p class="text-success text-center">
+                            <?php
+                            $error = $this->session->flashdata('error');
+                            if($error)
+                            {
+                            ?>
+                            <div class="alert alert-danger alert-dismissable" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <?php echo $error; ?>                    
+                            </div>
+                            <?php
+                            }
+                            $success = $this->session->flashdata('success');
+                            if($success)
+                            {
+                            ?>
+                            <div class="alert alert-success alert-dismissable">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <?php echo $success; ?>                    
+                            </div>
+                            <?php } ?>
+                        </p>
+                        <div class="form-group">
+                            <label class="col-sm-3 control-label">Nama</label>
+                            <div class="col-sm-6">
+                                <input type="text" class="form-control" id="fnama" name="fnama" placeholder="Nama" required="required">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- /.box-body -->
+                    <div class="box-footer">
+                        <button type="submit" class="btn btn-info pull-right">Submit</button>
+                    </div>
+                    <!-- /.box-footer -->
+                </form>
+                <!-- /.form -->
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<script type="text/javascript">
+    $(document).ready(function() {
+
+    });
+</script>
