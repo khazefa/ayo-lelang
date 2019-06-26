3c8407b574e6ca9424a9c07a2ff44eec1a047dab
Sigit Prayitno <cybergitt@gmail.com>
2019-06-26 20:00:34 +0700

    Create create.php

---
+++ application/modules/admin/views/pelelang/create.php
@@ -0,0 +1,92 @@
+<!-- Main content -->
+<section class="content">
+	<div class="row">
+		<div class="col-md-8 offset-md-4">
+			<div class="box box-default">
+				<div class="box-header with-border">
+					<a href="javascript: history.go(-1)"><i class="fa fa-reply"></i></a>
+					<h3 class="box-title"><?= $contentTitle; ?></h3>
+
+					<div class="box-tools pull-right">
+						<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+					</div>
+				</div>
+				<!-- /.box-header -->
+				<form class="form-horizontal" action="<?php echo base_url('admin/pelelang/create'); ?>" method="POST" role="form">
+					<div class="box-body">
+						<p class="text-success text-center">
+							<?php
+							$error = $this->session->flashdata('error');
+							if ($error) {
+								?>
+								<div class="alert alert-danger alert-dismissable" role="alert">
+									<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+									<?php echo $error; ?>
+								</div>
+							<?php
+						}
+						$success = $this->session->flashdata('success');
+						if ($success) {
+							?>
+								<div class="alert alert-success alert-dismissable">
+									<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+									<?php echo $success; ?>
+								</div>
+							<?php } ?>
+						</p>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Username</label>
+							<div class="col-sm-6">
+								<input type="text" class="form-control" id="fusername" name="fusername" placeholder="Username" required="required">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Password</label>
+							<div class="col-sm-6">
+								<input type="password" class="form-control" id="fpassword" name="fpassword" palceholder="Password">
+								<p class="help-block">Harap gunakan kombinasi huruf dan angka</p>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Nama</label>
+							<div class="col-sm-6">
+								<input type="text" class="form-control" id="fnama" name="fnama" placeholder="Nama" required="required">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Email</label>
+							<div class="col-sm-6">
+								<input type="email" class="form-control" id="femail" name="femail" placeholder="Email" required="required">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">No Telepon</label>
+							<div class="col-sm-6">
+								<input type="number" class="form-control" id="ftelepon" name="ftelepon" placeholder="No Telepon">
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-sm-3 control-label">Alamat</label>
+							<div class="col-sm-6">
+								<textarea id="falamat" name="falamat" class="form-control" placeholder="Alamat"></textarea>
+							</div>
+						</div>
+					</div>
+					<!-- /.box-body -->
+					<div class="box-footer">
+						<button type="submit" class="btn btn-info pull-right">Submit</button>
+					</div>
+					<!-- /.box-footer -->
+				</form>
+				<!-- /.form -->
+			</div>
+		</div>
+	</div>
+
+</section>
+
+<script type="text/javascript">
+	$(document).ready(function() {
+
+	});
+</script>
