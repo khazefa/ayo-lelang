b0137d5aea27d69d9c1f2dc5fa0103268b50710d
Sigit Prayitno <cybergitt@gmail.com>
2019-06-26 20:36:38 +0700

    Create Peserta.php

---
+++ application/modules/admin/controllers/Peserta.php
@@ -0,0 +1,169 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Peserta extends Back_Controller
+{
+	private $view_dir = 'admin/peserta/';
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->isLoggedIn();
+		$this->load->model('Peserta_model', 'MPeserta');
+	}
+
+	public function index()
+	{
+		$rs = array();
+		$arrWhere = array();
+		$arrOrder = array('email_peserta' => 'ASC');
+		$limit = 0;
+
+		$this->global['pageTitle'] = 'Peserta';
+		$this->global['contentHeader'] = 'Peserta';
+		$this->global['contentTitle'] = 'Peserta';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$rs = $this->MPeserta->get_data($arrWhere, $arrOrder, $limit);
+		$data['records'] = $rs;
+		$this->digiAdminLayout($data, $this->view_dir . 'index', $this->global);
+	}
+
+	public function add()
+	{
+		$this->global['pageTitle'] = "Tambah Data";
+		$this->global['contentHeader'] = 'Tambah Data';
+		$this->global['contentTitle'] = 'Tambah Data';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$data = array();
+		$this->digiAdminLayout($data, $this->view_dir . 'create', $this->global);
+	}
+
+	public function create()
+	{
+		$fusername = $this->input->post('fusername', TRUE);
+		$fpassword = $this->input->post('fpassword', TRUE);
+		$password = sha1($fpassword);
+		$fnama = $this->input->post('fnama', TRUE);
+		$femail = $this->input->post('femail', TRUE);
+		$ftelepon = $this->input->post('ftelepon', TRUE);
+		$falamat = $this->input->post('falamat', TRUE);
+		$tgl_daftar = date('Y-m-d');
+
+		$dataInfo = array('akun_peserta' => $fusername, 'sandi_peserta' => $password, 'nama_peserta' => $fnama, 'email_peserta' => $femail, 'telepon_peserta' => $ftelepon, 'alamat_peserta' => $falamat, 'tgl_daftar_peserta' => $tgl_daftar);
+		$result = $this->MPeserta->insert_data($dataInfo);
+
+		if ($result > 0) {
+			setFlashData('success', 'Data telah sukses ditambahkan');
+			redirect('admin/peserta');
+		} else {
+			setFlashData('error', 'Data telah gagal ditambahkan');
+			redirect('admin/peserta/add');
+		}
+	}
+
+	public function edit($fkey = NULL)
+	{
+		if ($fkey == NULL) {
+			redirect('admin');
+		}
+
+		$this->global['pageTitle'] = "Edit Data";
+		$this->global['contentHeader'] = 'Edit Data';
+		$this->global['contentTitle'] = 'Edit Data';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$rs = $this->MPeserta->get_data_info($fkey);
+		$data['records'] = $rs;
+		$this->digiAdminLayout($data, $this->view_dir . 'edit', $this->global);
+	}
+
+	public function profil($fkey = NULL)
+	{
+		if ($fkey == NULL) {
+			redirect('admin');
+		}
+
+		$this->global['pageTitle'] = "Edit Profil";
+		$this->global['contentHeader'] = 'Edit Profil';
+		$this->global['contentTitle'] = 'Edit Profil';
+		$this->global['name'] = $this->accName;
+		$this->global['role'] = $this->accRole;
+
+		$rs = $this->MPeserta->get_data_info($fkey);
+		$data['records'] = $rs;
+		$this->digiAdminLayout($data, $this->view_dir . 'profil', $this->global);
+	}
+
+	public function update_profil()
+	{
+		$fusername = $this->input->post('fusername', TRUE);
+		$fpassword = $this->input->post('fpassword', TRUE);
+		$fnama = $this->input->post('fnama', TRUE);
+		$femail = $this->input->post('femail', TRUE);
+		$ftelepon = $this->input->post('ftelepon', TRUE);
+		$falamat = $this->input->post('falamat', TRUE);
+
+		if (!empty($fpassword)) {
+			$password = sha1($fpassword);
+			$dataInfo = array('sandi_peserta' => $password, 'nama_peserta' => $fnama, 'email_peserta' => $femail, 'telepon_peserta' => $ftelepon, 'alamat_peserta' => $falamat);
+		} else {
+			$dataInfo = array('nama_peserta' => $fnama, 'email_peserta' => $femail, 'telepon_peserta' => $ftelepon, 'alamat_peserta' => $falamat);
+		}
+
+		$result = $this->MPeserta->update_data($dataInfo, $fusername);
+		if ($result == true) {
+			setFlashData('success', 'Data is successfully updated');
+			redirect('admin/peserta/profil/' . $fusername);
+		} else {
+			setFlashData('error', 'Failed to update data');
+			redirect('admin/peserta/profil' . $fusername);
+		}
+	}
+
+	public function update()
+	{
+		$fusername = $this->input->post('fusername', TRUE);
+		$fpassword = $this->input->post('fpassword', TRUE);
+		$fnama = $this->input->post('fnama', TRUE);
+		$femail = $this->input->post('femail', TRUE);
+		$ftelepon = $this->input->post('ftelepon', TRUE);
+		$falamat = $this->input->post('falamat', TRUE);
+
+		if (!empty($fpassword)) {
+			$password = sha1($fpassword);
+			$dataInfo = array('sandi_peserta' => $password, 'nama_peserta' => $fnama, 'email_peserta' => $femail, 'telepon_peserta' => $ftelepon, 'alamat_peserta' => $falamat);
+		} else {
+			$dataInfo = array('nama_peserta' => $fnama, 'email_peserta' => $femail, 'telepon_peserta' => $ftelepon, 'alamat_peserta' => $falamat);
+		}
+
+		$result = $this->MPeserta->update_data($dataInfo, $fusername);
+		if ($result == true) {
+			setFlashData('success', 'Data is successfully updated');
+			redirect('admin/peserta');
+		} else {
+			setFlashData('error', 'Failed to update data');
+			redirect('admin/peserta');
+		}
+	}
+
+	/**
+	 * This function is used to delete the data
+	 * @return boolean $result : TRUE / FALSE
+	 */
+	function delete($id = NULL)
+	{
+		$result = $this->MPeserta->delete_data($id);
+
+		if ($result > 0) {
+			setFlashData('success', 'Data is successfully deleted');
+		} else {
+			setFlashData('error', 'Failed to delete data');
+		}
+		redirect('pelelang');
+	}
+}
