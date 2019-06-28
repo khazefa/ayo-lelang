0e6f5a8483f45b327c0123191901d9ba36075da2
Sigit Prayitno <cybergitt@gmail.com>
2019-06-28 08:10:00 +0700

    Create Site.php

---
+++ application/modules/store/controllers/Site.php
@@ -0,0 +1,39 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+/**
+ * Class Site.php.
+ * Desc: Extending Front_Controller. Check /core/Front_Controller
+ * @author: Sigit Prayitno
+ * @email: cybergitt@gmail.com
+ */
+
+class Site extends Front_Controller
+{
+	private $view_dir = 'store/home';
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->isLoggedIn();
+		$this->load->model('store/Kategori_model', 'MKategori');
+	}
+
+	/**
+	 * Show Front interface
+	 */
+	public function index()
+	{
+		$this->global['pageTitle'] = 'Lelang Barang Online';
+		$this->global['contentTitle'] = 'Lelang Barang Online';
+		$this->global['name'] = $this->uName;
+
+		$rs = array();
+		$arrWhere = array();
+		$arrOrder = array('nama_kategori' => 'ASC');
+		$limit = 0;
+
+		$rs = $this->MKategori->get_data($arrWhere, $arrOrder, $limit);
+		$data['records_kategori'] = $rs;
+		$this->global['categories'] = $rs;
+		$this->digiLayout($data, $this->view_dir, $this->global);
+	}
+}
