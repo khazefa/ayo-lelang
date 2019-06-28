c0e7e20ebad581fd14be85155bbf0e3cdfb02364
Sigit Prayitno <cybergitt@gmail.com>
2019-06-28 08:10:08 +0700

    Update header.php

--- application/views/layouts/admin/_partials/header.php
+++ application/views/layouts/admin/_partials/header.php
@@ -69,7 +69,7 @@ desired effect
 		<header class="main-header">
 
 			<!-- Logo -->
-			<a href="index2.html" class="logo">
+			<a href="<?= base_url('admin/dashboard'); ?>" class="logo">
 				<!-- mini logo for sidebar mini 50x50 pixels -->
 				<span class="logo-mini"><b>A</b>LE</span>
 				<!-- logo for regular state and mobile devices -->
