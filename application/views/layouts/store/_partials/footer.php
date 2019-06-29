eadafc0ebf6d1255da1a3f71f7979388467b2927
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 13:58:50 +0700

    Update footer.php

--- application/views/layouts/store/_partials/footer.php
+++ application/views/layouts/store/_partials/footer.php
@@ -1,25 +1,27 @@
-  </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-  	<div class="container">
-  		<div class="pull-right hidden-xs">
-  			<?= SITE_NAME; ?>
-  		</div>
-  		<strong>Copyright &copy; 2019 <a href="#">Salman</a>.</strong> All rights reserved.
-  	</div>
-  	<!-- /.container -->
-  </footer>
-  </div>
-  <!-- ./wrapper -->
+		</div>
+		<!-- /.container -->
+	</div>
+	<!-- /.content-wrapper -->
+	<footer class="main-footer">
+		<div class="container">
+			<div class="pull-right hidden-xs">
+				<?= SITE_NAME; ?>
+			</div>
+			<strong>Copyright &copy; 2019 <a href="#">Salman</a>.</strong> All rights reserved.
+		</div>
+		<!-- /.container -->
+	</footer>
+	</div>
+	<!-- ./wrapper -->
 
-  <!-- SlimScroll -->
-  <script src="<?= base_url('assets/jquery-slimscroll/jquery.slimscroll.min.js'); ?>"></script>
-  <!-- FastClick -->
-  <script src="<?= base_url('assets/fastclick/lib/fastclick.js'); ?>"></script>
-  <!-- AdminLTE App -->
-  <script src="<?= base_url('assets/js/adminlte.min.js'); ?>"></script>
-  <!-- AdminLTE for demo purposes -->
-  <!-- <script src="../../dist/js/demo.js"></script> -->
-  </body>
+	<!-- SlimScroll -->
+	<script src="<?= base_url('assets/jquery-slimscroll/jquery.slimscroll.min.js'); ?>"></script>
+	<!-- FastClick -->
+	<script src="<?= base_url('assets/fastclick/lib/fastclick.js'); ?>"></script>
+	<!-- AdminLTE App -->
+	<script src="<?= base_url('assets/js/adminlte.min.js'); ?>"></script>
+	<!-- AdminLTE for demo purposes -->
+	<!-- <script src="../../dist/js/demo.js"></script> -->
+	</body>
 
-  </html>
+	</html>
