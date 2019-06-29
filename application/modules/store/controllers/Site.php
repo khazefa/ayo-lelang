a0f4f7182b4b7b1cdef53c672ca11bd25ceac0c8
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 14:50:49 +0700

    Update Site.php

--- application/modules/store/controllers/Site.php
+++ application/modules/store/controllers/Site.php
@@ -36,4 +36,16 @@ class Site extends Front_Controller
 		$this->global['categories'] = $rs;
 		$this->digiLayout($data, $this->view_dir, $this->global);
 	}
+	/**
+	 * Show Front Cara Lelang
+	 */
+	public function cara_lelang()
+	{
+		$this->global['pageTitle'] = 'Tata Cara Lelang - ' . SITE_NAME;
+		$this->global['contentTitle'] = 'Tata Cara Lelang - ' . SITE_NAME;
+		$this->global['name'] = $this->uName;
+
+		$data = array();
+		$this->digiLayout($data, 'store/pages/cara-lelang', $this->global);
+	}
 }
