0e849ec6663172d73c29cc858ae248b3ce023879
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 17:27:35 +0700

    Update home.php

--- application/modules/store/views/home.php
+++ application/modules/store/views/home.php
@@ -1,144 +1,193 @@
-      <!-- Main content -->
-      <section class="content">
-      	<div class="box box-solid">
-      		<div class="box-header with-border text-center">
-      			<h3 class="box-title">Katalog Lelang Pekan Ini</h3>
-      		</div>
-      		<div class="box-body">
-      			<div class="col-md-3">
-      				<div class="box box-warning box-solid">
-      					<div class="box-header with-border">
-      						<h3 class="box-title wrapping-text">Xiaomi A2</h3>
-      						<!-- /.box-tools -->
-      					</div>
-      					<!-- /.box-header -->
-      					<div class="box-body">
-      						<a href="#">
-      							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
-      						</a>
-      					</div>
-      					<div class="box-footer">
-      						<div class="pull-left">
-      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
-      						</div>
-      						<div class="pull-right">
-      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
-      						</div>
-      					</div>
-      					<!-- /.box-body -->
-      				</div>
-      				<!-- /.box -->
-      			</div>
-      			<!-- /.col -->
+<section class="content-header">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+		</ol>
 
-      			<div class="col-md-3">
-      				<div class="box box-warning box-solid">
-      					<div class="box-header with-border">
-      						<h3 class="box-title">Xiaomi A2</h3>
-      						<!-- /.box-tools -->
-      					</div>
-      					<!-- /.box-header -->
-      					<div class="box-body">
-      						<a href="#">
-      							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
-      						</a>
-      					</div>
-      					<div class="box-footer">
-      						<div class="pull-left">
-      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
-      						</div>
-      						<div class="pull-right">
-      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
-      						</div>
-      					</div>
-      					<!-- /.box-body -->
-      				</div>
-      				<!-- /.box -->
-      			</div>
-      			<!-- /.col -->
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner">
 
-      			<div class="col-md-3">
-      				<div class="box box-warning box-solid">
-      					<div class="box-header with-border">
-      						<h3 class="box-title">Xiaomi A2</h3>
-      						<!-- /.box-tools -->
-      					</div>
-      					<!-- /.box-header -->
-      					<div class="box-body">
-      						<a href="#">
-      							<img class="img-responsive img-carbox" src="https://via.placeholder.com/240x480.webp/09f/fff" alt="Photo">
-      						</a>
-      					</div>
-      					<div class="box-footer">
-      						<div class="pull-left">
-      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
-      						</div>
-      						<div class="pull-right">
-      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
-      						</div>
-      					</div>
-      					<!-- /.box-body -->
-      				</div>
-      				<!-- /.box -->
-      			</div>
-      			<!-- /.col -->
+			<div class="item active">
+				<img src="https://via.placeholder.com/728x240.webp/09f/fff" alt="Los Angeles" style="width:100%;">
+				<div class="carousel-caption">
+					<h3>Los Angeles</h3>
+					<p>LA is always so much fun!</p>
+				</div>
+			</div>
 
-      			<div class="col-md-3">
-      				<div class="box box-warning box-solid">
-      					<div class="box-header with-border">
-      						<h3 class="box-title">Xiaomi A2</h3>
-      						<!-- /.box-tools -->
-      					</div>
-      					<!-- /.box-header -->
-      					<div class="box-body">
-      						<a href="#">
-      							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
-      						</a>
-      					</div>
-      					<div class="box-footer">
-      						<div class="pull-left">
-      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
-      						</div>
-      						<div class="pull-right">
-      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
-      						</div>
-      					</div>
-      					<!-- /.box-body -->
-      				</div>
-      				<!-- /.box -->
-      			</div>
-      			<!-- /.col -->
+			<div class="item">
+				<img src="https://via.placeholder.com/728x240.webp/09f/fff" alt="Chicago" style="width:100%;">
+				<div class="carousel-caption">
+					<h3>Chicago</h3>
+					<p>Thank you, Chicago!</p>
+				</div>
+			</div>
 
-      			<div class="col-md-3">
-      				<div class="box box-warning box-solid">
-      					<div class="box-header with-border">
-      						<h3 class="box-title">Xiaomi A2</h3>
-      						<!-- /.box-tools -->
-      					</div>
-      					<!-- /.box-header -->
-      					<div class="box-body">
-      						<a href="#">
-      							<img class="img-responsive img-carbox" src="https://via.placeholder.com/240x480.webp/09f/fff" alt="Photo">
-      						</a>
-      					</div>
-      					<div class="box-footer">
-      						<div class="pull-left">
-      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
-      						</div>
-      						<div class="pull-right">
-      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
-      						</div>
-      					</div>
-      					<!-- /.box-body -->
-      				</div>
-      				<!-- /.box -->
-      			</div>
-      			<!-- /.col -->
-      		</div>
-      		<!-- /.box-body -->
-      	</div>
-      	<!-- /.box -->
-      </section>
-      <!-- /.content -->
-      </div>
-      <!-- /.container -->
+			<div class="item">
+				<img src="https://via.placeholder.com/728x240.webp/09f/fff" alt="New York" style="width:100%;">
+				<div class="carousel-caption">
+					<h3>New York</h3>
+					<p>We love the Big Apple!</p>
+				</div>
+			</div>
+
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div>
+</section>
+<!-- Main content -->
+<section class="content">
+	<div class="box box-solid">
+		<div class="box-header with-border text-center">
+			<h3 class="box-title">Katalog Lelang Pekan Ini</h3>
+		</div>
+		<div class="box-body">
+			<div class="col-md-3">
+				<div class="box box-warning box-solid">
+					<div class="box-header with-border">
+						<h3 class="box-title wrapping-text">Xiaomi A2</h3>
+						<!-- /.box-tools -->
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<a href="#">
+							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+						</a>
+					</div>
+					<div class="box-footer">
+						<div class="pull-left">
+							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+						</div>
+						<div class="pull-right">
+							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+						</div>
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
+			<!-- /.col -->
+
+			<div class="col-md-3">
+				<div class="box box-warning box-solid">
+					<div class="box-header with-border">
+						<h3 class="box-title">Xiaomi A2</h3>
+						<!-- /.box-tools -->
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<a href="#">
+							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+						</a>
+					</div>
+					<div class="box-footer">
+						<div class="pull-left">
+							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+						</div>
+						<div class="pull-right">
+							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+						</div>
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
+			<!-- /.col -->
+
+			<div class="col-md-3">
+				<div class="box box-warning box-solid">
+					<div class="box-header with-border">
+						<h3 class="box-title">Xiaomi A2</h3>
+						<!-- /.box-tools -->
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<a href="#">
+							<img class="img-responsive img-carbox" src="https://via.placeholder.com/240x480.webp/09f/fff" alt="Photo">
+						</a>
+					</div>
+					<div class="box-footer">
+						<div class="pull-left">
+							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+						</div>
+						<div class="pull-right">
+							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+						</div>
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
+			<!-- /.col -->
+
+			<div class="col-md-3">
+				<div class="box box-warning box-solid">
+					<div class="box-header with-border">
+						<h3 class="box-title">Xiaomi A2</h3>
+						<!-- /.box-tools -->
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<a href="#">
+							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+						</a>
+					</div>
+					<div class="box-footer">
+						<div class="pull-left">
+							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+						</div>
+						<div class="pull-right">
+							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+						</div>
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
+			<!-- /.col -->
+
+			<div class="col-md-3">
+				<div class="box box-warning box-solid">
+					<div class="box-header with-border">
+						<h3 class="box-title">Xiaomi A2</h3>
+						<!-- /.box-tools -->
+					</div>
+					<!-- /.box-header -->
+					<div class="box-body">
+						<a href="#">
+							<img class="img-responsive img-carbox" src="https://via.placeholder.com/240x480.webp/09f/fff" alt="Photo">
+						</a>
+					</div>
+					<div class="box-footer">
+						<div class="pull-left">
+							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+						</div>
+						<div class="pull-right">
+							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+						</div>
+					</div>
+					<!-- /.box-body -->
+				</div>
+				<!-- /.box -->
+			</div>
+			<!-- /.col -->
+		</div>
+		<!-- /.box-body -->
+	</div>
+	<!-- /.box -->
+</section>
+<!-- /.content -->
+</div>
+<!-- /.container -->
