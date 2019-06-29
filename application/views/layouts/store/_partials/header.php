d749c12aac85268ea89e43d43e329354498a9acc
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 13:58:53 +0700

    Update header.php

--- application/views/layouts/store/_partials/header.php
+++ application/views/layouts/store/_partials/header.php
@@ -36,7 +36,7 @@
 </head>
 <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
 
-<body class="hold-transition skin-blue layout-top-nav">
+<body class="hold-transition skin-green layout-top-nav">
 	<div class="wrapper">
 
 		<header class="main-header">
@@ -52,7 +52,6 @@
 					<!-- Collect the nav links, forms, and other content for toggling -->
 					<div class="collapse navbar-collapse pull-left" id="navbar-collapse">
 						<ul class="nav navbar-nav">
-							<li><a href="<?= base_url(); ?>">Home</a></li>
 							<li><a href="<?= base_url('tata-cara-lelang'); ?>">Tata Cara Lelang</a></li>
 							<li class="dropdown">
 								<a href="#" class="dropdown-toggle" data-toggle="dropdown">Kategori Lelang <span class="caret"></span></a>
@@ -100,51 +99,46 @@
 								</ul>
 							</li>
 						</ul>
-						<ul class="nav navbar-nav navbar-right">
+						<ul class="nav navbar-nav">
 							<?php
-							if ( $this->session->userdata('signed_in') ) {
-
-							} else {
-							?>
-							<li>
-								<p class="navbar-text">Already have an account?</p>
-							</li>
-							<li class="dropdown">
-								<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-								<ul id="login-dp" class="dropdown-menu">
-									<li>
-										<div class="row">
-											<div class="col-md-12">
-												<form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-													<div class="form-group">
-														<label class="sr-only" for="exampleInputEmail2">Email address</label>
-														<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-													</div>
-													<div class="form-group">
-														<label class="sr-only" for="exampleInputPassword2">Password</label>
-														<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-														<div class="help-block text-right"><a href="">Forget the password ?</a></div>
-													</div>
-													<div class="form-group">
-														<button type="submit" class="btn btn-primary btn-block">Sign in</button>
-													</div>
-													<div class="checkbox">
-														<label>
-															<input type="checkbox"> keep me logged-in
-														</label>
-													</div>
-												</form>
+							if ($this->session->userdata('signed_in')) { } else {
+								?>
+								<li class="dropdown">
+									<a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
+									<ul id="login-dp" class="dropdown-menu">
+										<li>
+											<div class="row">
+												<div class="col-md-12">
+													<form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
+														<div class="form-group">
+															<label class="sr-only" for="exampleInputEmail2">Email address</label>
+															<input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+														</div>
+														<div class="form-group">
+															<label class="sr-only" for="exampleInputPassword2">Password</label>
+															<input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+															<div class="help-block text-right"><a href="">Forget the password ?</a></div>
+														</div>
+														<div class="form-group">
+															<button type="submit" class="btn btn-primary btn-block">Sign in</button>
+														</div>
+														<div class="checkbox">
+															<label>
+																<input type="checkbox"> keep me logged-in
+															</label>
+														</div>
+													</form>
+												</div>
+												<div class="bottom text-center">
+													New here ? <a href="#"><b>Join Us</b></a>
+												</div>
 											</div>
-											<div class="bottom text-center">
-												New here ? <a href="#"><b>Join Us</b></a>
-											</div>
-										</div>
-									</li>
-								</ul>
-							</li>
+										</li>
+									</ul>
+								</li>
 							<?php
-							}
-							?>
+						}
+						?>
 						</ul>
 					</div>
 					<!-- /.navbar-custom-menu -->
@@ -169,3 +163,52 @@
 					</ol>
 				</section>
 				-->
+				<section class="content-header">
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+						<!-- Indicators -->
+						<ol class="carousel-indicators">
+							<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+							<li data-target="#myCarousel" data-slide-to="1"></li>
+							<li data-target="#myCarousel" data-slide-to="2"></li>
+						</ol>
+
+						<!-- Wrapper for slides -->
+						<div class="carousel-inner">
+
+							<div class="item active">
+								<img src="https://via.placeholder.com/728x240.webp/09f/fff" alt="Los Angeles" style="width:100%;">
+								<div class="carousel-caption">
+									<h3>Los Angeles</h3>
+									<p>LA is always so much fun!</p>
+								</div>
+							</div>
+
+							<div class="item">
+								<img src="https://via.placeholder.com/728x240.webp/09f/fff" alt="Chicago" style="width:100%;">
+								<div class="carousel-caption">
+									<h3>Chicago</h3>
+									<p>Thank you, Chicago!</p>
+								</div>
+							</div>
+
+							<div class="item">
+								<img src="https://via.placeholder.com/728x240.webp/09f/fff" alt="New York" style="width:100%;">
+								<div class="carousel-caption">
+									<h3>New York</h3>
+									<p>We love the Big Apple!</p>
+								</div>
+							</div>
+
+						</div>
+
+						<!-- Left and right controls -->
+						<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+							<span class="glyphicon glyphicon-chevron-left"></span>
+							<span class="sr-only">Previous</span>
+						</a>
+						<a class="right carousel-control" href="#myCarousel" data-slide="next">
+							<span class="glyphicon glyphicon-chevron-right"></span>
+							<span class="sr-only">Next</span>
+						</a>
+					</div>
+				</section>
