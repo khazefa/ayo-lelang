f8f07b0d56dce9a574a625b41581101ba94eeb2b
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 17:27:38 +0700

    Create cara-lelang.php

---
+++ application/modules/store/views/pages/cara-lelang.php
@@ -0,0 +1,19 @@
+	<!-- Main content -->
+	<section class="content">
+		<div class="box box-solid">
+			<div class="box-header with-border text-center">
+				<h3 class="box-title">Tata Cara Lelang</h3>
+			</div>
+			<div class="box-body">
+				<p>
+					rehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+					wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+					eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+					assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+					nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+					farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+					labore sustainable VHS.
+				</p>
+			</div>
+		</div>
+	</section>
