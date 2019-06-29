26a7639a2492216ffb4a409f1d2f7f3ddb744e9d
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 13:58:45 +0700

    Create home.1.php

---
+++ application/modules/store/views/home.1.php
@@ -0,0 +1,134 @@
+      <!-- Main content -->
+      <section class="content">
+      	<div class="box box-solid">
+      		<div class="box-header with-border text-center">
+      			<h3 class="box-title">Katalog Lelang Pekan Ini</h3>
+      		</div>
+      		<div class="box-body">
+      			<div class="col-md-3">
+      				<div class="box box-solid">
+      					<div class="box-header with-border">
+      						<h3 class="box-title">Xiaomi A2</h3>
+      						<!-- /.box-tools -->
+      					</div>
+      					<!-- /.box-header -->
+      					<div class="box-body">
+      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      					</div>
+      					<div class="box-footer">
+      						<div class="pull-left">
+      							<p class="text-default">IDR 2500.000</p>
+      						</div>
+      						<div class="pull-right">
+      							<button type="button" class="btn btn-primary">Bid</button>
+      						</div>
+      					</div>
+      					<!-- /.box-body -->
+      				</div>
+      				<!-- /.box -->
+      			</div>
+      			<!-- /.col -->
+
+      			<div class="col-md-3">
+      				<div class="box">
+      					<div class="box-header with-border">
+      						<h3 class="box-title">Xiaomi A2</h3>
+      						<!-- /.box-tools -->
+      					</div>
+      					<!-- /.box-header -->
+      					<div class="box-body">
+      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      					</div>
+      					<div class="box-footer">
+      						<div class="pull-left">
+      							<p class="text-default">IDR 2500.000</p>
+      						</div>
+      						<div class="pull-right">
+      							<button type="button" class="btn btn-primary">Bid</button>
+      						</div>
+      					</div>
+      					<!-- /.box-body -->
+      				</div>
+      				<!-- /.box -->
+      			</div>
+      			<!-- /.col -->
+
+      			<div class="col-md-3">
+      				<div class="box">
+      					<div class="box-header with-border">
+      						<h3 class="box-title">Xiaomi A2</h3>
+      						<!-- /.box-tools -->
+      					</div>
+      					<!-- /.box-header -->
+      					<div class="box-body">
+      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      					</div>
+      					<div class="box-footer">
+      						<div class="pull-left">
+      							<p class="text-default">IDR 2500.000</p>
+      						</div>
+      						<div class="pull-right">
+      							<button type="button" class="btn btn-primary">Bid</button>
+      						</div>
+      					</div>
+      					<!-- /.box-body -->
+      				</div>
+      				<!-- /.box -->
+      			</div>
+      			<!-- /.col -->
+
+      			<div class="col-md-3">
+      				<div class="box">
+      					<div class="box-header with-border">
+      						<h3 class="box-title">Xiaomi A2</h3>
+      						<!-- /.box-tools -->
+      					</div>
+      					<!-- /.box-header -->
+      					<div class="box-body">
+      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      					</div>
+      					<div class="box-footer">
+      						<div class="pull-left">
+      							<p class="text-default">IDR 2500.000</p>
+      						</div>
+      						<div class="pull-right">
+      							<button type="button" class="btn btn-primary">Bid</button>
+      						</div>
+      					</div>
+      					<!-- /.box-body -->
+      				</div>
+      				<!-- /.box -->
+      			</div>
+      			<!-- /.col -->
+
+      			<div class="col-md-3">
+      				<div class="box">
+      					<div class="box-header with-border">
+      						<h3 class="box-title">Xiaomi A2</h3>
+      						<!-- /.box-tools -->
+      					</div>
+      					<!-- /.box-header -->
+      					<div class="box-body">
+      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      					</div>
+      					<div class="box-footer">
+      						<div class="pull-left">
+      							<p class="text-default">IDR 2500.000</p>
+      						</div>
+      						<div class="pull-right">
+      							<button type="button" class="btn btn-primary">Bid</button>
+      						</div>
+      					</div>
+      					<!-- /.box-body -->
+      				</div>
+      				<!-- /.box -->
+      			</div>
+      			<!-- /.col -->
+      		</div>
+      		<!-- /.box-body -->
+      	</div>
+      	<!-- /.box -->
+      </section>
+      <!-- /.content -->
+      </div>
+      <!-- /.container -->
