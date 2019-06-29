734cd346488d478f4ae9c2b976881525ff8573ee
Sigit Prayitno <cybergitt@gmail.com>
2019-06-29 13:58:47 +0700

    Update home.php

--- application/modules/store/views/home.php
+++ application/modules/store/views/home.php
@@ -1,26 +1,28 @@
       <!-- Main content -->
       <section class="content">
-      	<div class="box box-default">
-      		<div class="box-header with-border">
-      			<h3 class="box-title">Katalog Lelang</h3>
+      	<div class="box box-solid">
+      		<div class="box-header with-border text-center">
+      			<h3 class="box-title">Katalog Lelang Pekan Ini</h3>
       		</div>
       		<div class="box-body">
       			<div class="col-md-3">
-      				<div class="box">
+      				<div class="box box-warning box-solid">
       					<div class="box-header with-border">
-      						<h3 class="box-title">Xiaomi A2</h3>
+      						<h3 class="box-title wrapping-text">Xiaomi A2</h3>
       						<!-- /.box-tools -->
       					</div>
       					<!-- /.box-header -->
       					<div class="box-body">
-      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						<a href="#">
+      							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						</a>
       					</div>
       					<div class="box-footer">
       						<div class="pull-left">
-      							<p class="text-default">IDR 2500.000</p>
+      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
       						</div>
       						<div class="pull-right">
-      							<button type="button" class="btn btn-primary">Bid</button>
+      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
       						</div>
       					</div>
       					<!-- /.box-body -->
@@ -30,21 +32,23 @@
       			<!-- /.col -->
 
       			<div class="col-md-3">
-      				<div class="box">
+      				<div class="box box-warning box-solid">
       					<div class="box-header with-border">
       						<h3 class="box-title">Xiaomi A2</h3>
       						<!-- /.box-tools -->
       					</div>
       					<!-- /.box-header -->
       					<div class="box-body">
-      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						<a href="#">
+      							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						</a>
       					</div>
       					<div class="box-footer">
       						<div class="pull-left">
-      							<p class="text-default">IDR 2500.000</p>
+      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
       						</div>
       						<div class="pull-right">
-      							<button type="button" class="btn btn-primary">Bid</button>
+      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
       						</div>
       					</div>
       					<!-- /.box-body -->
@@ -54,21 +58,23 @@
       			<!-- /.col -->
 
       			<div class="col-md-3">
-      				<div class="box">
+      				<div class="box box-warning box-solid">
       					<div class="box-header with-border">
       						<h3 class="box-title">Xiaomi A2</h3>
       						<!-- /.box-tools -->
       					</div>
       					<!-- /.box-header -->
       					<div class="box-body">
-      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						<a href="#">
+      							<img class="img-responsive img-carbox" src="https://via.placeholder.com/240x480.webp/09f/fff" alt="Photo">
+      						</a>
       					</div>
       					<div class="box-footer">
       						<div class="pull-left">
-      							<p class="text-default">IDR 2500.000</p>
+      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
       						</div>
       						<div class="pull-right">
-      							<button type="button" class="btn btn-primary">Bid</button>
+      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
       						</div>
       					</div>
       					<!-- /.box-body -->
@@ -78,21 +84,23 @@
       			<!-- /.col -->
 
       			<div class="col-md-3">
-      				<div class="box">
+      				<div class="box box-warning box-solid">
       					<div class="box-header with-border">
       						<h3 class="box-title">Xiaomi A2</h3>
       						<!-- /.box-tools -->
       					</div>
       					<!-- /.box-header -->
       					<div class="box-body">
-      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						<a href="#">
+      							<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						</a>
       					</div>
       					<div class="box-footer">
       						<div class="pull-left">
-      							<p class="text-default">IDR 2500.000</p>
+      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
       						</div>
       						<div class="pull-right">
-      							<button type="button" class="btn btn-primary">Bid</button>
+      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
       						</div>
       					</div>
       					<!-- /.box-body -->
@@ -102,21 +110,23 @@
       			<!-- /.col -->
 
       			<div class="col-md-3">
-      				<div class="box">
+      				<div class="box box-warning box-solid">
       					<div class="box-header with-border">
       						<h3 class="box-title">Xiaomi A2</h3>
       						<!-- /.box-tools -->
       					</div>
       					<!-- /.box-header -->
       					<div class="box-body">
-      						<img class="img-responsive pad" src="<?= base_url('uploads/products/xiaomi_a2.jpeg'); ?>" alt="Photo">
+      						<a href="#">
+      							<img class="img-responsive img-carbox" src="https://via.placeholder.com/240x480.webp/09f/fff" alt="Photo">
+      						</a>
       					</div>
       					<div class="box-footer">
       						<div class="pull-left">
-      							<p class="text-default">IDR 2500.000</p>
+      							<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
       						</div>
       						<div class="pull-right">
-      							<button type="button" class="btn btn-primary">Bid</button>
+      							<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
       						</div>
       					</div>
       					<!-- /.box-body -->
