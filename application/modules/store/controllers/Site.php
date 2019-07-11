06a570e5c56de09d2cf7c67fadd3097f6d3f7eae
Sigit Prayitno <cybergitt@gmail.com>
2019-07-11 23:15:25 +0700

    Update Site.php

--- application/modules/store/controllers/Site.php
+++ application/modules/store/controllers/Site.php
@@ -14,7 +14,10 @@ class Site extends Front_Controller
 	{
 		parent::__construct();
 		$this->isLoggedIn();
+		//load libary pagination
+		$this->load->library('pagination');
 		$this->load->model('store/Kategori_model', 'MKategori');
+		$this->load->model('store/Produk_model', 'MProduk');
 	}
 
 	/**
@@ -26,15 +29,71 @@ class Site extends Front_Controller
 		$this->global['contentTitle'] = 'Lelang Barang Online';
 		$this->global['name'] = $this->uName;
 
-		$rs = array();
+		$rs_kategori = array();
+		$rs_produk = array();
 		$arrWhere = array();
-		$arrOrder = array('nama_kategori' => 'ASC');
-		$limit = 0;
+		$start = 0;
+		$limit = 12;
 
-		$rs = $this->MKategori->get_data($arrWhere, $arrOrder, $limit);
-		$data['records_kategori'] = $rs;
+		$rs_kategori = $this->MKategori->get_data($arrWhere, array('nama_kategori' => 'ASC'), $limit);
+		$data['records_kategori'] = $rs_kategori;
+		$rs_produk = $this->MProduk->get_data($arrWhere, array('waktu_selesai' => 'ASC'), $limit, $start);
+		$data['records_produk'] = $rs_produk;
 		$this->digiLayout($data, $this->view_dir, $this->global);
 	}
+
+	public function list_json( $row_num = 0 )
+	{
+		$rs_produk = array();
+		$arrWhere = array();
+		$limit = 4;
+
+		if ($row_num !== 0) {
+			$row_num = ($row_num - 1) * $limit;
+		}
+
+		//panggil function get_mahasiswa_list yang ada pada mmodel mahasiswa_model. 
+		$rs_produk = $this->MProduk->get_data($arrWhere, array('waktu_selesai' => 'ASC'), $limit, $row_num);
+		$data['records_produk'] = $rs_produk;
+
+		//konfigurasi pagination
+		$config['base_url'] = base_url() . 'site/list_json';
+		$config['total_rows'] = $this->MProduk->count_all(); //total row
+		$config['per_page'] = $limit;  //show record per halaman
+		$config["uri_segment"] = 3;  // uri parameter
+		$choice = $config["total_rows"] / $config["per_page"];
+		$config["num_links"] = floor($choice);
+
+		// Membuat Style pagination untuk BootStrap v4
+		$config['first_link']       = 'First';
+		$config['last_link']        = 'Last';
+		$config['next_link']        = 'Next';
+		$config['prev_link']        = 'Prev';
+		$config['full_tag_open']    = '<div class="pagging text-center"><nav><ul class="pagination justify-content-center">';
+		$config['full_tag_close']   = '</ul></nav></div>';
+		$config['num_tag_open']     = '<li class="page-item"><span class="page-link">';
+		$config['num_tag_close']    = '</span></li>';
+		$config['cur_tag_open']     = '<li class="page-item active"><span class="page-link">';
+		$config['cur_tag_close']    = '<span class="sr-only">(current)</span></span></li>';
+		$config['next_tag_open']    = '<li class="page-item"><span class="page-link">';
+		$config['next_tagl_close']  = '<span aria-hidden="true">&raquo;</span></span></li>';
+		$config['prev_tag_open']    = '<li class="page-item"><span class="page-link">';
+		$config['prev_tagl_close']  = '</span>Next</li>';
+		$config['first_tag_open']   = '<li class="page-item"><span class="page-link">';
+		$config['first_tagl_close'] = '</span></li>';
+		$config['last_tag_open']    = '<li class="page-item"><span class="page-link">';
+		$config['last_tagl_close']  = '</span></li>';
+
+		$this->pagination->initialize($config);
+		$data['page'] = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+		$data['pagination'] = $this->pagination->create_links();
+
+		$data['row'] = $row_num;
+
+		echo json_encode($data);
+	}
+
 	/**
 	 * Show Front Cara Lelang
 	 */
