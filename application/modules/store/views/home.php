9eb1ff3d71fa0db6fa19ae6023f5ab1702f622bc
Sigit Prayitno <cybergitt@gmail.com>
2019-07-13 23:46:32 +0700

    Update home.php

--- application/modules/store/views/home.php
+++ application/modules/store/views/home.php
@@ -56,33 +56,45 @@
 		<div class="box-body">
 			<div class="row">
 				<?php
-				foreach ($rs_produk as $rp) {
-					?>
-					<div class="col-md-3">
-						<div class="box box-warning box-solid">
-							<div class="box-header with-border">
-								<h3 class="box-title wrapping-text"><?= $rp['nama_lelang']; ?></h3>
-								<!-- /.box-tools -->
-							</div>
-							<!-- /.box-header -->
-							<div class="box-body">
-								<a href="#">
-									<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/' . $rp['gambar_produk']); ?>" alt="<?= $rp['nama_lelang']; ?>">
-								</a>
-							</div>
-							<div class="box-footer">
-								<div class="pull-left">
-									<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+				if (count($rs_produk) > 0) {
+					foreach ($rs_produk as $rp) {
+						?>
+						<div class="col-md-3">
+							<div class="box box-warning box-solid">
+								<div class="box-header with-border">
+									<h3 class="box-title wrapping-text"><?= $rp['nama_lelang']; ?></h3>
+									<!-- /.box-tools -->
 								</div>
-								<div class="pull-right">
-									<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+								<!-- /.box-header -->
+								<div class="box-body">
+									<a href="#">
+										<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/' . $rp['gambar_produk']); ?>" alt="<?= $rp['nama_lelang']; ?>">
+									</a>
 								</div>
+								<div class="box-footer">
+									<div class="pull-left">
+										<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
+									</div>
+									<div class="pull-right">
+										<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+									</div>
+								</div>
+								<!-- /.box-body -->
 							</div>
-							<!-- /.box-body -->
+							<!-- /.box -->
 						</div>
-						<!-- /.box -->
+						<!-- /.col -->
+					<?php
+					}
+				} else {
+					?>
+					<div class="col-md-12">
+						<div class="alert alert-info">
+							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+							Maaf belum ada produk untuk dilelang pada hari ini.
+						</div>
+
 					</div>
-					<!-- /.col -->
 				<?php
 				}
 				?>
