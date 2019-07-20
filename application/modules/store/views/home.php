d4959ecb7f9dacdc4c749da9695456c9a8b1c916
Sigit Prayitno <cybergitt@gmail.com>
2019-07-20 16:39:06 +0700

    Update home.php

--- application/modules/store/views/home.php
+++ application/modules/store/views/home.php
@@ -58,7 +58,11 @@
 				<?php
 				if (count($rs_produk) > 0) {
 					foreach ($rs_produk as $rp) {
-						?>
+						$id = (int) $rp['id_lelang'];
+						$short_desc = text_shorter($rp['keterangan'], 75);
+						$price = (int) $rp['harga_awal'];
+						$price_idr = format_rupiah($price);
+					?>
 						<div class="col-md-3">
 							<div class="box box-warning box-solid">
 								<div class="box-header with-border">
@@ -70,13 +74,17 @@
 									<a href="<?= base_url('produk/detail/'.$rp['id_lelang']); ?>">
 										<img class="img-responsive img-carbox" src="<?= base_url('uploads/products/' . $rp['gambar_produk']); ?>" alt="<?= $rp['nama_lelang']; ?>">
 									</a>
+                            		<p class="text-default"><?= $short_desc; ?></p>
+									<div class="price">
+										<h5>Mulai dari <strong class="text-danger"><?= $price_idr; ?></strong></h5>
+									</div>
 								</div>
 								<div class="box-footer">
 									<div class="pull-left">
 										<button type="button" class="btn btn-success btn-block">Buy It Now!</button>
 									</div>
 									<div class="pull-right">
-										<button type="button" class="btn btn-danger btn-block">Bid Now!</button>
+										<button type="button" class="btn btn-danger btn-block" data-toggle="modal" data-target="#modal-bid" data-id="<?= $id; ?>" data-price="<?= $price; ?>">Bid Now!</button>
 									</div>
 								</div>
 								<!-- /.box-body -->
