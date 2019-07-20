7ac98968574d290ec957cc6f776f22bdee4e8bae
Sigit Prayitno <cybergitt@gmail.com>
2019-07-20 16:39:01 +0700

    Update any_code_helper.php

--- application/helpers/any_code_helper.php
+++ application/helpers/any_code_helper.php
@@ -132,4 +132,25 @@ if (!function_exists('indonesian_date')) {
 		return $date;
 	}
 }
+
+if (!function_exists('text_shorter')) {
+	function text_shorter($text, $chars_limit)
+	{
+		// Check if length is larger than the character limit
+		if (strlen($text) > $chars_limit)
+		{
+			// If so, cut the string at the character limit
+			$new_text = substr($text, 0, $chars_limit);
+			// Trim off white space
+			$new_text = trim($new_text);
+			// Add at end of text ...
+			return $new_text . "...";
+		}
+		// If not just return the text as is
+		else
+		{
+		return $text;
+		}
+	}
+}
 ?>
