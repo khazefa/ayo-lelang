f9ee774450ce168124597776fa3e09ced3fea813
Sigit Prayitno <cybergitt@gmail.com>
2019-07-21 18:06:21 +0700

    Update Bid.php

--- application/modules/store/controllers/Bid.php
+++ application/modules/store/controllers/Bid.php
@@ -17,6 +17,7 @@ class Bid extends Front_Controller
 		if ($this->session->userdata('signed_in')) {
 			$this->load->model('store/Tawaran_model', 'MBid');
 		} else {
+			setFlashData('error', 'Anda harus login terlebih dahulu.');
 			redirect('login');
 			exit;
 		}
@@ -46,16 +47,60 @@ class Bid extends Front_Controller
 			'id_peserta' => $peserta_id, 'id_lelang' => $item_id, 'jumlah_tawaran' => $bid_price, 'waktu_tawaran' => $current_date
 		);
 
-		$result = $this->MBid->insert_data($dataInfo);
+		$arrWhere = array('id_peserta' => $peserta_id, 'id_lelang' => $item_id);
+		$count = $this->MBid->check_data_exists($arrWhere);
 
-		if ($result > 0) {
-			setFlashData('success', $peserta_name . ', Bid Anda telah terinput di sistem kami.');
-			redirect('peserta/status-bid');
-		} else {
-			setFlashData('error', $peserta_name . ', Bid Anda gagal terinput di sistem kami.');
+		if ( $count > 0 ) {
+			setFlashData('error', 'Anda sudah melakukan Bid untuk produk tersebut.');
 			redirect('/');
+		} else {
+			$result = $this->MBid->insert_data($dataInfo);
+			if ($result > 0) {
+				setFlashData('success', $peserta_name . ', Bid Anda telah terinput di sistem kami.');
+				redirect('peserta/status-bid');
+			} else {
+				setFlashData('error', $peserta_name . ', Bid Anda gagal terinput di sistem kami.');
+				redirect('/');
+			}
 		}
+	}
 
+	/**
+	 * This function is used to delete the data
+	 * @return boolean $result : TRUE / FALSE
+	 */
+	function up_data()
+	{
+		$id = $this->input->post('mdl_up_bid_id', TRUE);
+		$bid_price = (int) $this->input->post('mdl_up_bid_price', TRUE);
+		$current_date = date('Y-m-d H:i:s');
+
+		$dataInfo = array(
+			'jumlah_tawaran' => $bid_price, 'waktu_tawaran' => $current_date
+		);
+
+		$result = $this->MBid->update_data($dataInfo, $id);
+		if ($result > 0) {
+			setFlashData('success', 'Bid Anda telah sukses diupdate');
+		} else {
+			setFlashData('error', 'Bid Anda telah gagal diupdate');
+		}
+		redirect('peserta/status-bid');
+	}
+
+	/**
+	 * This function is used to delete the data
+	 * @return boolean $result : TRUE / FALSE
+	 */
+	function del_data($fid)
+	{
+		$result = $this->MBid->delete_data($fid);
+		if ($result > 0) {
+			setFlashData('success', 'Data is successfully deleted');
+		} else {
+			setFlashData('error', 'Failed to delete data');
+		}
+		redirect('peserta/status-bid');
 	}
 	
 }
