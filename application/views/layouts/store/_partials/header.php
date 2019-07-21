19f10caa48e92cd0a8e6bb8ca85e027f11a17669
Sigit Prayitno <cybergitt@gmail.com>
2019-07-21 18:06:51 +0700

    Update header.php

--- application/views/layouts/store/_partials/header.php
+++ application/views/layouts/store/_partials/header.php
@@ -13,6 +13,8 @@
 	<link rel="stylesheet" href="<?= base_url('assets/font-awesome/css/font-awesome.min.css'); ?>">
 	<!-- Ionicons -->
 	<link rel="stylesheet" href="<?= base_url('assets/Ionicons/css/ionicons.min.css'); ?>">
+	<!-- DataTables -->
+	<link rel="stylesheet" href="<?= base_url('assets/datatables.net-bs/css/dataTables.bootstrap.css'); ?>">
 	<!-- Theme style -->
 	<link rel="stylesheet" href="<?= base_url('assets/css/AdminLTE.min.css'); ?>">
 	<link rel="stylesheet" href="<?= base_url('assets/css/custom.css'); ?>">
