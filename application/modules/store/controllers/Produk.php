7c96e94c7ea3e65af3acc9ed4c29b6773a2453cd
Sigit Prayitno <cybergitt@gmail.com>
2019-08-11 14:06:49 +0700

    Update Produk.php

--- application/modules/store/controllers/Produk.php
+++ application/modules/store/controllers/Produk.php
@@ -19,6 +19,7 @@ class Produk extends Front_Controller
 		$this->load->model('store/Pelelang_model', 'MPelelang');
 		$this->load->model('store/Kota_model', 'MKota');
 		$this->load->model('store/Tawaran_model', 'MTawaran');
+		$this->load->model('store/Peserta_model', 'MPeserta');
 	}
 
 	/**
@@ -44,6 +45,7 @@ class Produk extends Front_Controller
 		$rs_kategori = $this->MKategori->get_data_info2($rs_produk[0]["id_kategori"]);
 		$rs_pelelang = $this->MPelelang->get_data_info2($rs_produk[0]["id_pelelang"]);
 		$rs_kota = $this->MKota->get_data_info($rs_pelelang[0]["id_kota"]);
+		$rs_bid = $this->MTawaran->get_data(array('id_lelang' => $fkey), array('waktu_tawaran' => 'ASC'), 0, 0);
 
 		$this->global['pageTitle'] = 'Lelang ' . $rs_produk[0]["nama_lelang"];
 		$this->global['contentTitle'] = 'Lelang ' . $rs_produk[0]["nama_lelang"];
@@ -68,6 +70,23 @@ class Produk extends Front_Controller
 		$data['rs_kategori'] = $rs_kategori;
 		$data['rs_pelelang'] = $rs_pelelang;
 		$data['rs_kota'] = $rs_kota;
+
+		$arr_bid = array();
+		foreach ($rs_bid as $rp) {
+			$rs_peserta = $this->MPeserta->get_data_info2((int) $rp['id_peserta']);
+			$row['id'] = $rp['id_tawaran'];
+			$row['user_id'] = $rp['id_peserta'];
+			$row['user'] = $rs_peserta[0]['nama_peserta'];
+			$row['item_id'] = $rp['id_lelang'];
+			$row['bid_price'] = 'Rp. '.format_rupiah($rp['jumlah_tawaran']);
+			// $row['bid_time'] = indonesian_date($rp['waktu_tawaran']);
+			$row['bid_time'] = date('d/m/Y H:i:s', strtotime($rp['waktu_tawaran']));
+			$row['bid_type'] = strtoupper($rp['tipe_tawaran']);
+			$row['bid_status'] = strtoupper($rp['status_tawaran']);
+
+			array_push($arr_bid, $row);
+		}
+		$data['rs_bid'] = $arr_bid;
 
 		$this->digiLayout($data, $this->view_dir . "/detail", $this->global);
 	}
