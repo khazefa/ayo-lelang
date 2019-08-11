b033d29f1b46d0f30ef6a9585cf769ed0bf5937b
Sigit Prayitno <cybergitt@gmail.com>
2019-08-11 14:06:36 +0700

    Create custom-admin.js

---
+++ assets/js/custom-admin.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+	$.fn.dataTable.moment('DD/MM/YYYY');
+
+	$('#data-produk').dataTable({
+		"dom": "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+		"responsive": true,
+		"order": [[1, "asc"]]
+	});
+
+	$('#data-bid').dataTable({
+		"dom": "<'row'<'col-sm-12 col-md-6'l><'col-sm-12 col-md-6'f>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+		"responsive": true,
+		"order": [[4, "asc"]]
+	});
+
+	$('#data-saldo').dataTable({
+		"dom": "<'row'<'col-sm-12 col-md-6'Bl><'col-sm-12 col-md-6'f>>" +
+			"<'row'<'col-sm-12'tr>>" +
+			"<'row'<'col-sm-12 col-md-5'i><'col-sm-12 col-md-7'p>>",
+		"responsive": true,
+		"lengthChange": false,
+		"buttons": ['copy', 'excel', 'pdf', 'print'],
+		"order": [[1, "asc"]]
+	});
+});
