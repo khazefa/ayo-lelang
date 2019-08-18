0d945aab7e159f9ba580f8fb6ad11c48b5051cd6
Sigit Prayitno <cybergitt@gmail.com>
2019-08-18 21:08:53 +0700

    Update checkout.php

--- application/modules/store/views/peserta/checkout.php
+++ application/modules/store/views/peserta/checkout.php
@@ -19,53 +19,56 @@
 			<div class="box-body">
 				<div class="col-md-12">
 
-                    <div class="shopping_cart">
-                        <form class="form-horizontal" role="form" action="" method="post" id="payment-form">
-                            <div class="panel-group" id="accordion">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading">
-                                        <h4 class="panel-title">
-                                            <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Review
-                                                Your Order</a>
-                                        </h4>
-                                    </div>
-                                    <div id="collapseOne" class="panel-collapse collapse in">
-                                        <div class="panel-body">
-                                            <div class="items">
-                                                <div class="col-md-9">
-                                                    <table class="table table-striped">
-                                                        <tr>
-                                                            <td colspan="2">
-                                                                <a class="btn btn-warning btn-sm pull-right"
-                                                                   href="#"
-                                                                   title="Cancel Item">X</a>
-                                                                <strong>
+					<div class="shopping_cart">
+						<form class="form-horizontal" role="form" action="" method="post" id="payment-form">
+							<div class="panel-group" id="accordion">
+								<div class="panel panel-default">
+									<div class="panel-heading">
+										<h4 class="panel-title">
+											<a data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Review
+												Your Order</a>
+										</h4>
+									</div>
+									<div id="collapseOne" class="panel-collapse collapse in">
+										<div class="panel-body">
+											<div class="items">
+												<div class="col-md-9">
+													<table class="table table-striped">
+														<?php
+														$item_name =
+															$records_produk[0]['nama_lelang'];
+														$item_price = $records_bid[0]['jumlah_tawaran'];
+														$item_price_rp = "Rp. " . format_rupiah($item_price);
+														?>
+														<tr>
+															<td colspan="2">
+																<!--
+																<a class="btn btn-warning btn-sm pull-right" href="#" title="Cancel Item">X</a>
+																-->
+																<strong>
 																	Detail Order</strong>
 															</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>
-																<?=
-																	$records_produk[0]['nama_lelang'];
-																?>
-                                                            </td>
-                                                            <td>
+														</tr>
+														<tr>
+															<td>
+																<?= $item_name; ?>
+															</td>
+															<td>
 																<strong>
-																	
+																	<?= $item_price_rp; ?>
 																</strong>
-                                                                <b>$147.00</b>
-                                                            </td>
-                                                        </tr>
+															</td>
+														</tr>
 													</table>
 													<table class="table table-striped">
-                                                        <tr>
-                                                            <td colspan="2">
+														<tr>
+															<td colspan="2">
 																<strong>Shipping Detail</strong>
 															</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <td>
-															<?php
+														</tr>
+														<tr>
+															<td>
+																<?php
 																echo nl2br($records_peserta[0]['alamat_peserta']);
 																echo "<br>";
 																echo $records_kota[0]['nama_kota'];
@@ -73,128 +76,134 @@
 
 																$ongkir = $records_ongkir[0]['jumlah_biaya_kirim'];
 																$ongkir_rp = format_rupiah($ongkir);
-															?>
-                                                            </td>
-                                                            <td>
+																?>
+															</td>
+															<td>
 																Shipping Fee<br>
-                                                                <strong>Rp. <?= $ongkir_rp; ?></strong>
-                                                            </td>
-                                                        </tr>
+																<strong>Rp. <?= $ongkir_rp; ?></strong>
+															</td>
+														</tr>
 													</table>
-													
-                                                </div>
-                                                <div class="col-md-3">
-                                                    <div style="text-align: center;">
-                                                        <h3>Order Total</h3>
-                                                        <h3><span style="color:green;">$147.00</span></h3>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                </div>
+
+												</div>
+												<div class="col-md-3">
+													<div style="text-align: center;">
+														<?php
+														$total = $item_price + $ongkir;
+														$total_rp = "Rp. " . format_rupiah($total);
+														?>
+														<h3>Order Total</h3>
+														<h3><span style="color:green;"><?= $total_rp; ?></span></h3>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+
+								</div>
 							</div>
 
-                            <div class="panel panel-default">
-                                <div class="panel-heading">
-                                    <h4 class="panel-title">
-                                        <a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
-                                            <b>Payment Information</b>
-                                        </a>
-                                    </h4>
-                                </div>
-                                <div id="collapseThree" class="panel-collapse collapse">
-                                    <div class="panel-body">
-                                        <span class='payment-errors'></span>
-                                        <fieldset>
-                                            <legend>What method would you like to pay with today?</legend>
-                                            <div class="form-group">
-                                                <label class="col-sm-3 control-label" for="card-holder-name">Name on
-                                                    Card</label>
-                                                <div class="col-sm-9">
-                                                    <input type="text" class="form-control" stripe-data="name"
-                                                           id="name-on-card" placeholder="Card Holder's Name">
-                                                </div>
-                                            </div>
-                                            <div class="form-group">
-                                                <label class="col-sm-3 control-label" for="card-number">Card
-                                                    Number</label>
-                                                <div class="col-sm-9">
-                                                    <input type="text" class="form-control" stripe-data="number"
-                                                           id="card-number" placeholder="Debit/Credit Card Number">
-                                                    <br/>
-                                                    <div><img class="pull-right"
-                                                              src="https://s3.amazonaws.com/hiresnetwork/imgs/cc.png"
-                                                              style="max-width: 250px; padding-bottom: 20px;">
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label class="col-sm-3 control-label" for="expiry-month">Expiration
-                                                        Date</label>
-                                                    <div class="col-sm-9">
-                                                        <div class="row">
-                                                            <div class="col-xs-3">
-                                                                <select class="form-control col-sm-2"
-                                                                        data-stripe="exp-month" id="card-exp-month"
-                                                                        style="margin-left:5px;">
-                                                                    <option>Month</option>
-                                                                    <option value="01">Jan (01)</option>
-                                                                    <option value="02">Feb (02)</option>
-                                                                    <option value="03">Mar (03)</option>
-                                                                    <option value="04">Apr (04)</option>
-                                                                    <option value="05">May (05)</option>
-                                                                    <option value="06">June (06)</option>
-                                                                    <option value="07">July (07)</option>
-                                                                    <option value="08">Aug (08)</option>
-                                                                    <option value="09">Sep (09)</option>
-                                                                    <option value="10">Oct (10)</option>
-                                                                    <option value="11">Nov (11)</option>
-                                                                    <option value="12">Dec (12)</option>
-                                                                </select>
-                                                            </div>
-                                                            <div class="col-xs-3">
-                                                                <select class="form-control" data-stripe="exp-year"
-                                                                        id="card-exp-year">
-                                                                    <option value="2016">2016</option>
-                                                                    <option value="2017">2017</option>
-                                                                    <option value="2018">2018</option>
-                                                                    <option value="2019">2019</option>
-                                                                    <option value="2020">2020</option>
-                                                                    <option value="2021">2021</option>
-                                                                    <option value="2022">2022</option>
-                                                                    <option value="2023">2023</option>
-                                                                    <option value="2024">2024</option>
-                                                                </select>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <label class="col-sm-3 control-label" for="cvv">Card CVC</label>
-                                                    <div class="col-sm-3">
-                                                        <input type="text" class="form-control" stripe-data="cvc"
-                                                               id="card-cvc" placeholder="Security Code">
-                                                    </div>
-                                                </div>
-                                                <div class="form-group">
-                                                    <div class="col-sm-offset-3 col-sm-9">
-                                                    </div>
-                                                </div>
-                                        </fieldset>
-                                        <button type="submit" class="btn btn-success btn-lg" style="width:100%;">Pay
-                                            Now
-                                        </button>
-                                        <br/>
-                                        <div style="text-align: left;"><br/>
-                                            By submiting this order you are agreeing to our <a href="/legal/billing/">universal
-                                                billing agreement</a>, and <a href="/legal/terms/">terms of service</a>.
-                                            If you have any questions about our products or services please contact us
-                                            before placing this order.
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+							<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a data-toggle="collapse" data-parent="#accordion" href="#collapseThree">
+											<b>Payment Information</b>
+										</a>
+									</h4>
+								</div>
+								<div id="collapseThree" class="panel-collapse collapse">
+									<div class="panel-body">
+										<span class='payment-errors'></span>
+										<fieldset>
+											<legend>Bank Transfer</legend>
+											<p>
+												Kami menerima pembayaran dari berbagai Bank di Indonesia dengan melalui berbagai cara, diantaranya adalah melalui Internet Banking, Transfer ATM, m-Banking, SMS Banking, Setoran Tunai maupun Phone Banking. Semua pembayaran produk lelang dapat dilakukan melalui rekening bank berikut:
+											</p>
+
+											<div class="row">
+												<!-- 
+													Panel Bank Transfer Info
+												-->
+												<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div class="panel panel-primary">
+														<div class="panel-heading text-center">
+															<img src="<?= base_url('assets/img/bank/bca.png'); ?>">
+														</div>
+														<div class="panel-body">
+															<ul>
+																<li>No. Rekening: 0283116411</li>
+																<li>a.n: AYO LELANG</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+
+												<!-- 
+													Panel Bank Transfer Info
+												-->
+												<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div class="panel panel-primary">
+														<div class="panel-heading text-center">
+															<img src="<?= base_url('assets/img/bank/bri.png'); ?>">
+														</div>
+														<div class="panel-body">
+															<ul>
+																<li>No. Rekening: 040901000391111</li>
+																<li>a.n: AYO LELANG</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+											</div>
+
+											<div class="row">
+												<!-- 
+												Panel Bank Transfer Info
+											-->
+												<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div class="panel panel-primary">
+														<div class="panel-heading text-center">
+															<img src="<?= base_url('assets/img/bank/bni.png'); ?>">
+														</div>
+														<div class="panel-body">
+															<ul>
+																<li>No. Rekening: 3300031111</li>
+																<li>a.n: AYO LELANG</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+
+												<!-- 
+												Panel Bank Transfer Info
+											-->
+												<div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+													<div class="panel panel-primary">
+														<div class="panel-heading text-center">
+															<img src="<?= base_url('assets/img/bank/mandiri.png'); ?>">
+														</div>
+														<div class="panel-body">
+															<ul>
+																<li>No. Rekening: 1030006052222</li>
+																<li>a.n: AYO LELANG</li>
+															</ul>
+														</div>
+													</div>
+												</div>
+											</div>
+
+										</fieldset>
+										<!--
+										<button type="submit" class="btn btn-success btn-lg" style="width:100%;">Konfirmasi Pembayaran
+										</button>
+										-->
+										<br />
+										<div style="text-align: left;"><br />
+											Setelah melakukan pembayaran, silahkan konfirmasi pembayaran melalui Invoice Area > List Order > Pilih Tombol Konfirmasi pada Order Anda. Kami akan memproses order Anda setelah konfirmasi dilakukan.
+										</div>
+									</div>
+								</div>
+							</div>
 
 						</form>
 					</div>
